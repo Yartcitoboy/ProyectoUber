1af12e4e98cf8541c9e6f161660a426f81a898bd
Is4 <is.riquelmet@duocuc.cl>
2024-12-15 23:36:35 -0330

    Credenciales Genericas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,26 @@
+// export const environment = {
+//   production: true,
+//   firebaseConfig : {
+//     apiKey: "AIzaSyAccdfDzlzd3uSNa8j0AjhxJStwckhxj68",
+//     authDomain: "proyectouber-812da.firebaseapp.com",
+//     projectId: "proyectouber-812da",
+//     storageBucket: "proyectouber-812da.appspot.com",
+//     messagingSenderId: "594316297812",
+//     appId: "1:594316297812:web:7264c143254726f545cfc1",
+//     measurementId: "G-YY8FQC3QZC"
+//   }
+// };
+
+
 export const environment = {
   production: true,
-  firebaseConfig : {
-    apiKey: "AIzaSyAccdfDzlzd3uSNa8j0AjhxJStwckhxj68",
-    authDomain: "proyectouber-812da.firebaseapp.com",
-    projectId: "proyectouber-812da",
-    storageBucket: "proyectouber-812da.appspot.com",
-    messagingSenderId: "594316297812",
-    appId: "1:594316297812:web:7264c143254726f545cfc1",
-    measurementId: "G-YY8FQC3QZC"
-  }
-};
+  firebaseConfig: {
+  apiKey: 'API_KEY_FIREBASE_CAMBIAR',
+  authDomain: 'AUTH_DOMAIN_FIREBASE_CAMBIAR',
+  projectId: 'PROJECT_ID_FIREBASE_CAMBIAR',
+  storageBucket: 'STORAGE_BUCKET_FIREBASE_CAMBIAR',
+  messagingSenderId: 'SENDER_ID_FIREBASE_CAMBIAR',
+  appId: 'APP_ID_FIREBASE_CAMBIAR',
+  measurementId: 'MEASUREMENT_ID_FIREBASE_CAMBIAR'
+}
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,26 +1,31 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+
+// export const environment = {
+//   production: false,
+//   firebaseConfig : {
+//     apiKey: "AIzaSyAccdfDzlzd3uSNa8j0AjhxJStwckhxj68",
+//     authDomain: "proyectouber-812da.firebaseapp.com",
+//     projectId: "proyectouber-812da",
+//     storageBucket: "proyectouber-812da.appspot.com",
+//     messagingSenderId: "594316297812",
+//     appId: "1:594316297812:web:7264c143254726f545cfc1",
+//     measurementId: "G-YY8FQC3QZC"
+//   },
+//   googleMapsApiKey: 'AIzaSyAccdfDzlzd3uSNa8j0AjhxJStwckhxj68'
+// };
 
 export const environment = {
   production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyAccdfDzlzd3uSNa8j0AjhxJStwckhxj68",
-    authDomain: "proyectouber-812da.firebaseapp.com",
-    projectId: "proyectouber-812da",
-    storageBucket: "proyectouber-812da.appspot.com",
-    messagingSenderId: "594316297812",
-    appId: "1:594316297812:web:7264c143254726f545cfc1",
-    measurementId: "G-YY8FQC3QZC"
+  firebaseConfig: {
+    apiKey: 'API_KEY_FIREBASE_CAMBIAR',
+    authDomain: 'AUTH_DOMAIN_FIREBASE_CAMBIAR',
+    databaseURL: 'DATABASE_URL_FIREBASE_CAMBIAR',
+    projectId: 'PROJECT_ID_FIREBASE_CAMBIAR',
+    storageBucket: 'STORAGE_BUCKET_FIREBASE_CAMBIAR',
+    messagingSenderId: 'SENDER_ID_FIREBASE_CAMBIAR',
+    appId: 'APP_ID_FIREBASE_CAMBIAR',
+    measurementId: 'MEASUREMENT_ID_FIREBASE_CAMBIAR'
   },
-  googleMapsApiKey: 'AIzaSyAccdfDzlzd3uSNa8j0AjhxJStwckhxj68'
+  googleMapsApiKey: 'API_KEY_GOOGLE_MAPS_CAMBIAR' 
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
